58c2b6862bbf14641df522a2578927c7ce0cc5ce
John (Jack) Meyer <jack10042@gmail.com>
2020-10-03 23:58:41 -0400

    Update exports

--- packages/can-i-have-that/src/index.ts
+++ packages/can-i-have-that/src/index.ts
@@ -1,4 +1,6 @@
 export { GameStateIterator } from './game-state-iterator';
 export { defaultParams } from './game-params';
+export { StateTransformer } from './state-transformer';
+export { ResponseValidator } from './response-validator';
 export { LocalMaximumHandler as DefaultBotHandler } from './handlers/local-maximum-handler';
 export { IntermediaryHandler } from './handlers/intermediary-handler';
--- packages/hearts/src/index.ts
+++ packages/hearts/src/index.ts
@@ -1,4 +1,6 @@
 export { GameStateIterator } from './game-state-iterator';
 export { defaultParams } from './game-params';
+export { StateTransformer } from './state-transformer';
+export { ResponseValidator } from './response-validator';
 export { HeuristicHandler as DefaultBotHandler } from './handlers/heuristic-handler';
 export { IntermediaryHandler } from './handlers/intermediary-handler';
